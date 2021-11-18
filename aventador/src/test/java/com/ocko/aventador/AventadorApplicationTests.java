823a4cfd1eaedd301b3a63c35840bfa32dc5bd7e
ock <ocko112@naver.com>
2021-11-18 22:50:42 +0900

    20211118 프로젝트 생성

---
+++ aventador/src/test/java/com/ocko/aventador/AventadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ocko.aventador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AventadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

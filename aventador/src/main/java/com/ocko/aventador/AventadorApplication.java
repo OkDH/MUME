b0d0727c5b59c2bce8f1be5cb8ae79d2cd810616
ock <ocko112@naver.com>
2021-11-18 22:50:42 +0900

    20211118 프로젝트 생성

---
+++ aventador/src/main/java/com/ocko/aventador/AventadorApplication.java
@@ -0,0 +1,13 @@
+package com.ocko.aventador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AventadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AventadorApplication.class, args);
+	}
+
+}

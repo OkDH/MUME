1fd380f32ad202da5ff8c0ea88b9bebaa1a085ee
ok <ok@DESKTOP-R801POO>
2021-11-23 19:06:19 +0900

    20211123 template 생성 중

--- aventador/src/main/java/com/ocko/aventador/controller/PageController.java
+++ aventador/src/main/java/com/ocko/aventador/controller/PageController.java
@@ -1,8 +1,68 @@
 package com.ocko.aventador.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.ocko.aventador.dao.model.aventador.MemberAccount;
+import com.ocko.aventador.service.AuthenticationService;
 
 @Controller
 public class PageController {
 
+private final static Logger logger = LoggerFactory.getLogger(PageController.class);
+	
+	@Autowired private AuthenticationService authenticationService;
+	
+	@ModelAttribute(name = "memberId")
+	public Integer modelMember() {
+		MemberAccount memberAccount = authenticationService.getCurrentMember();
+		if(memberAccount == null)
+			return null;
+		return memberAccount.getMemberId();
+	}
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String getRoot() {
+		logger.info("redirect from:/ --> to:/public/");
+		return "redirect:/public/";
+	}
+	
+	@RequestMapping(value = "/{pathName:(?:public|private)}", method = RequestMethod.GET)
+	public String getPathIncorrectly (@PathVariable String pathName) {
+		logger.info("redirect from:/{} --> to:/{}/", pathName, pathName);
+		return "redirect:/" + pathName + "/";
+	}
+	
+	@RequestMapping(value = "/public/**", method = RequestMethod.GET)
+	public ModelAndView getPublic(HttpServletRequest servletRequest) {
+		String subpath = ((String)servletRequest.getAttribute( HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE )).substring("/public".length());
+		if(subpath.equals("")
+				|| subpath.endsWith("/")) {
+			subpath += "index";
+		}
+//		ModelAndView modelAndView = new ModelAndView("/public" + subpath);
+		ModelAndView modelAndView = new ModelAndView(subpath);
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/private/**", method = RequestMethod.GET)
+	public ModelAndView getPrivate(HttpServletRequest servletRequest) {
+		String subpath = ((String)servletRequest.getAttribute( HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE )).substring("/private".length());
+		if(subpath.equals("")
+				|| subpath.endsWith("/")) {
+			subpath += "index";
+		}
+		ModelAndView modelAndView = new ModelAndView("/private" + subpath);
+		return modelAndView;
+	}
 }

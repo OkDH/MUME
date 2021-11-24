7e1777ab1c4da35f2c42221210d2757c5e4debd9
ok <ok@DESKTOP-R801POO>
2021-11-24 19:01:10 +0900

    20211124 미국시장지수

---
+++ aventador/src/main/java/com/ocko/aventador/model/StockDetail.java
@@ -0,0 +1,170 @@
+/**
+ * 
+ */
+package com.ocko.aventador.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+/**
+ * @author ok
+ *
+ */
+public class StockDetail {
+	
+	private String symbol;
+	
+	private LocalDate date;
+	
+	private BigDecimal high;
+	private BigDecimal low;
+	private BigDecimal close;
+	private BigDecimal open;
+	
+	private BigDecimal prevClose;
+	
+	// 전일 대비 변화
+	private BigDecimal chg;
+	// 전일 대비 변화율
+	private double chgp;
+	
+	private double rsi;
+	
+	/**
+	 * 전일 대비 변화 계산
+	 * @return
+	 */
+	public BigDecimal getChg() {
+		if(close != null && prevClose != null) {
+			chg = close.subtract(prevClose);
+			return chg;
+		}
+		return null;
+	}
+	
+	/**
+	 * 전일 대비 변화율 계산
+	 * @return
+	 */
+	public double getChgp() {
+		getChg();
+		if(chg != null || prevClose != null) {
+			chgp = chg.divide(prevClose, 5, BigDecimal.ROUND_HALF_UP).movePointRight(2).setScale(2, BigDecimal.ROUND_HALF_UP).doubleValue();
+			return chgp;
+		}
+		return 0;
+	}
+
+	/**
+	 * @return {@link #symbol}
+	 */
+	public String getSymbol() {
+		return symbol;
+	}
+
+	/**
+	 * @param symbol {@link #symbol}
+	 */
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+
+	/**
+	 * @return {@link #date}
+	 */
+	public LocalDate getDate() {
+		return date;
+	}
+
+	/**
+	 * @param date {@link #date}
+	 */
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	/**
+	 * @return {@link #high}
+	 */
+	public BigDecimal getHigh() {
+		return high;
+	}
+
+	/**
+	 * @param high {@link #high}
+	 */
+	public void setHigh(BigDecimal high) {
+		this.high = high;
+	}
+
+	/**
+	 * @return {@link #low}
+	 */
+	public BigDecimal getLow() {
+		return low;
+	}
+
+	/**
+	 * @param low {@link #low}
+	 */
+	public void setLow(BigDecimal low) {
+		this.low = low;
+	}
+
+	/**
+	 * @return {@link #close}
+	 */
+	public BigDecimal getClose() {
+		return close;
+	}
+
+	/**
+	 * @param close {@link #close}
+	 */
+	public void setClose(BigDecimal close) {
+		this.close = close;
+	}
+
+	/**
+	 * @return {@link #open}
+	 */
+	public BigDecimal getOpen() {
+		return open;
+	}
+
+	/**
+	 * @param open {@link #open}
+	 */
+	public void setOpen(BigDecimal open) {
+		this.open = open;
+	}
+
+	/**
+	 * @return {@link #rsi}
+	 */
+	public double getRsi() {
+		return rsi;
+	}
+
+	/**
+	 * @param rsi {@link #rsi}
+	 */
+	public void setRsi(double rsi) {
+		this.rsi = rsi;
+	}
+
+	/**
+	 * @return {@link #prevClose}
+	 */
+	public BigDecimal getPrevClose() {
+		return prevClose;
+	}
+
+	/**
+	 * @param prevClose {@link #prevClose}
+	 */
+	public void setPrevClose(BigDecimal prevClose) {
+		this.prevClose = prevClose;
+	}
+	
+}
--- aventador/src/main/resources/static/js/app.js
+++ aventador/src/main/resources/static/js/app.js
@@ -1,3 +1,33 @@
+/**
+ * meta initializer (non-lazy loader)
+ * 식별: meta == window.meta
+ * 결과: meta[name] 중복시 array로 처리
+ */ 
+(function(){
+	meta = {};
+	var metaElements = [];
+	
+	read();
+	document.addEventListener("DOMContentLoaded", read);
+	
+	function read(){
+		var newMetaElements = [];
+		for (var i=0; i<document.getElementsByTagName("meta").length; i++){
+			if(metaElements.indexOf(document.getElementsByTagName("meta")[i]) < 0){
+				newMetaElements.push(document.getElementsByTagName("meta")[i]);
+			}
+		}
+		
+		newMetaElements.forEach(function(e){
+			var name = e.getAttribute("name");
+			var content = e.getAttribute("content");
+			
+			meta[name] = content;
+		});
+	}
+	
+})();
+
 var app = angular.module("app", ["ngRoute"]);
 app.config(["$routeProvider", "$httpProvider", 
 		function($routeProvider, $httpProvider) {
@@ -41,34 +71,127 @@ app.factory("responseObserver", ["$q", "$window", function($q, $window){
 	}
 }]);
 app.run(["$rootScope", "$location", function($rootScope, $location){
-//	$rootScope.meta = meta; 
-//	
-//	// USAGES:
-//	// meta.path(): to return current path
-//	// meta.path("/path/string"): to check whether current path is including(starting with) "/path/string"
-//	$rootScope.path = function(){
-//		// getter
-//		// return current path
-//		if(arguments.length == 0){
-//			return $location.path();
-//		}
-//		
-//		// checker
-//		// check current path pattern
-//		var path;
-//		for(var i=0; i<arguments.length; i++){
-//			path = arguments[i];
-//			if(typeof path === "string"){
-//				if($location.path().length >= path.length
-//						&& $location.path().substring(0, path.length) == path){
-//					return true;
-//				}
-//			}
-//		}
-//		
-//		return false;
-//	}
+	$rootScope.meta = meta; 
+	
+	// USAGES:
+	// meta.path(): to return current path
+	// meta.path("/path/string"): to check whether current path is including(starting with) "/path/string"
+	$rootScope.path = function(){
+		// getter
+		// return current path
+		if(arguments.length == 0){
+			return $location.path();
+		}
+		
+		// checker
+		// check current path pattern
+		var path;
+		for(var i=0; i<arguments.length; i++){
+			path = arguments[i];
+			if(typeof path === "string"){
+				if($location.path().length >= path.length
+						&& $location.path().substring(0, path.length) == path){
+					return true;
+				}
+			}
+		}
+		
+		return false;
+	}
 }]);
+app.service("httpService", function($http, $q){
+	var httpService = this;
+	
+	// request default
+	var requestDefault = {
+		method: "POST",
+		headers: {
+			"Accept": "application/json; charset=UTF-8",
+			"Content-Type": "application/json; charset=UTF-8",
+		}
+	}
+	
+	if(meta._csrf_header)
+		requestDefault.headers[meta._csrf_header] = meta._csrf;
+	
+	// promise repository
+	// [ [promise1, timeout1], [promise2, timeout2], ... ]
+	var arrayPromiseTimeout = [];
+	
+	// ----------------
+	// public functions
+	// ----------------
+	this.run = fnRequest;
+	this.request = fnRequest;
+	
+	this.post = function(request){
+		request.method = "POST";
+		return fnRequest(request);
+	}
+	this.get = function(request){
+		request.method = "GET";
+		delete request.data;
+		return fnRequest(request);
+	}
+	this.promises = function(){
+		return arrayPromiseTimeout.map(function(e){ return e[0]; });
+	}
+	
+	this.stop = function(promise){
+		if(!promise)
+			return; 
+		var idx = findPromiseIndex(promise);
+		if(idx >= 0){
+			arrayPromiseTimeout[idx][1].resolve();
+			arrayPromiseTimeout.splice(idx, 1);
+		}
+	}
+	
+	this.status403 = null;
+	
+	
+	// ----------------
+	// core functions
+	// ----------------
+	
+	function fnRequest(request){
+		var requestOption = angular.extend({}, requestDefault, request);
+		
+		var timeoutDefer = $q.defer();
+		requestOption.timeout = timeoutDefer.promise;
+		
+		var promise = $http(requestOption);
+		arrayPromiseTimeout.push([ promise, timeoutDefer ]);
+		
+		promise.then(function(){
+			var idx = findPromiseIndex(promise);
+			if(idx >= 0){
+				arrayPromiseTimeout.splice(idx, 1);
+			}
+		}, function(e){
+			console.log(e);
+			if(e.status == 403){
+				if(httpService.status403){
+					httpService.status403(e);
+				}else if(meta && meta.baseurl){
+					window.location.href = meta.baseurl;
+				}
+			}
+		});
+		
+		return promise;
+	}
+	
+	function findPromiseIndex(promise){
+		for(var i=0; i < arrayPromiseTimeout.length; i++){
+			if(arrayPromiseTimeout[i][0] === promise){
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+});
 app.controller("MainController", function($scope, $http, $location){
 	
 	

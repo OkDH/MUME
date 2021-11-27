56c86059251d60ed01f598bb754e471da75302fb
ock <ocko112@naver.com>
2021-11-27 22:42:19 +0900

    20211127 서버 DB, 업데이트 일자

--- aventador/src/main/java/com/ocko/aventador/scheduler/StockDataScheduler.java
+++ aventador/src/main/java/com/ocko/aventador/scheduler/StockDataScheduler.java
@@ -17,6 +17,7 @@ import java.util.Queue;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -24,7 +25,11 @@ import org.springframework.stereotype.Component;
 import com.ocko.aventador.component.StockComponent;
 import com.ocko.aventador.constant.EtfSymbol;
 import com.ocko.aventador.dao.model.aventador.StockHistory;
+import com.ocko.aventador.dao.model.aventador.StockHistoryExample;
+import com.ocko.aventador.dao.model.aventador.ViewTodayStock;
+import com.ocko.aventador.dao.model.aventador.ViewTodayStockExample;
 import com.ocko.aventador.dao.persistence.aventador.StockHistoryMapper;
+import com.ocko.aventador.dao.persistence.aventador.ViewTodayStockMapper;
 import com.ocko.aventador.model.StockDetail;
 
 import yahoofinance.Stock;
@@ -41,41 +46,106 @@ public class StockDataScheduler {
 	
 	private static final Logger log = LoggerFactory.getLogger(StockDataScheduler.class);
 	
-	@Autowired private StockHistoryMapper stockHistoryMapper;
 	@Autowired private StockComponent stockComponent;
+	@Autowired private StockHistoryMapper stockHistoryMapper;
+	@Autowired private ViewTodayStockMapper viewTodayStockMapper;
 	
 	/**
-	 * 월~토
-	 * 오후 5시 부터 다음날 오전 6시까지 5분마다 작동
-	 * 
+	 * 주가 데이터 수집 스케쥴러
+	 * 전체 프로세스 : 월~토 오후 5시 부터 다음날 오전 6시까지 5분마다 작동
+	 * schedulerUpdate1 : 월~금 오후 5시부터 11시 55분까지 5분마다 작동
+	 * schedulerUpdate2 : 화~토 오전 0시부터 5시 55분까지 5분마다 작동
+	 * schedulerUpdate3 : 화~토 오전 6시 5분에 작동(최종)
 	 */
 	@Scheduled(cron="0 0/5 17-23 * * MON-FRI")
-	public void updateStockHistory1() {
+	public void schedulerUpdate1() {
+		updateStocksHistory(LocalDate.now());
+	}
+	@Scheduled(cron="0 0/5 0-5 * * THU-SAT")
+	public void schedulerUpdate2() {
+		updateStocksHistory(LocalDate.now().minusDays(1)); // 하루 넘어갔으니 전일 날짜로 업데이트
+	}
+	@Scheduled(cron="0 5 6 * * THU-SAT")
+	public void schedulerUpdate3() {
+		updateStocksHistory(LocalDate.now().minusDays(1)); // 하루 넘어갔으니 전일 날짜로 업데이트
+	}
+	
+	/**
+	 * 심볼별로 주가 데이터를 수집
+	 * 1. 야후 파이낸스 API에서 각 심볼별로 주가 데이터를 가져옴
+	 * 2. ViewTodayStock에서 심볼별로 가장 최신주가 데이터를 가져옴
+	 * 3. viewTodayStock의 update_time과 파이낸스 데이터의 LastTradeTime과 비교
+	 * 4. LastTradeTime이 updateTime보다 나중이라면 업데이트
+	 * @param stockDate 주가 일자
+	 */
+	private void updateStocksHistory(LocalDate stockDate) {
+		Map<String, StockDetail> stocks = getStocksData();
+		if(stocks == null)
+			return;
 		
-//		// 심볼리스트
-//		List<String> symbolList = new ArrayList<String>();
-//		for(EtfSymbol symbol : EtfSymbol.values()) {
-//			symbolList.add(symbol.name());
-//		}
-//		
-//		// 심볼리스트 To 배열
-//		String[] symbols = symbolList.toArray(new String[symbolList.size()]);
-//		// 심볼 주가 복수 조회
-//		Map<String, StockDetail> stocks = stockComponent.getStocks(symbols);
-//		
-//		for(String symbol : stocks.keySet()) {
-//			
-//		}
+		for(String symbol : stocks.keySet()) {
+			StockDetail stock = stocks.get(symbol);
+			
+			// 2. ViewTodayStock에서 가장 최신을 가져옴.
+			ViewTodayStockExample viewExample = new ViewTodayStockExample();
+			viewExample.createCriteria().andSymbolEqualTo(stock.getSymbol());
+			List<ViewTodayStock> viewTodayStockList = viewTodayStockMapper.selectByExample(viewExample);
+			if(!viewTodayStockList.isEmpty()) {
+				ViewTodayStock viewTodayStock = viewTodayStockList.get(0);
+				
+				// 3. viewTodayStock의 update_time과 파이낸스 데이터의 LastTradeTime과 비교
+				// 4. LastTradeTime이 updateTime보다 나중이라면 업데이트
+				if(stock.getLastTradeTime().isAfter(viewTodayStock.getUpdateTime())) {
+					StockHistory stockHistory = new StockHistory();
+					
+					// 객체복사
+					BeanUtils.copyProperties(stock, stockHistory);
+					stockHistory.setStockDate(stockDate);
+					
+					// 전일 데이터 가져오기
+					StockHistoryExample example = new StockHistoryExample();
+					example.createCriteria().andSymbolEqualTo(stock.getSymbol()).andStockDateNotEqualTo(stockDate);
+					example.setOrderByClause("stock_date desc");
+					example.setLimit(1);
+					List<StockHistory> stockHistorys = stockHistoryMapper.selectByExample(example);
 
-		log.info("하이1");
+					if(!stockHistorys.isEmpty()) {
+						StockHistory yesterDayStock = stockHistorys.get(0);
+						
+						float up = stock.getChg() > 0 ? stock.getChg() : 0.0f;
+						float down = stock.getChg() < 0 ? stock.getChg()*(-1) : 0.0f;
+						
+						float upAvg = ((yesterDayStock.getUpAvg() * 13) + up) / 14;
+						float dwAvg = ((yesterDayStock.getDwAvg() * 13) + down) / 14;
+						float rsi = 100 - (100 / (1 + (upAvg / dwAvg)));
+						
+						stockHistory.setUpAvg(upAvg);
+						stockHistory.setDwAvg(dwAvg);
+						stockHistory.setRsi(rsi);
+						
+						stockHistory.setUpdateTime(LocalDateTime.now());
+						stockHistoryMapper.upsert(stockHistory);
+					}
+				}
+			}
+		}
 	}
 	
-	@Scheduled(cron="0 0/5 0-6 * * THU-SAT")
-	public void updateStockHistory2() {
-		log.info("하이2");
+	/**
+	 * 실시간 symbol 주가 데이터 가져오기
+	 * @return
+	 */
+	private Map<String, StockDetail> getStocksData(){
+		// 심볼리스트
+		List<String> symbolList = new ArrayList<String>();
+		for(EtfSymbol symbol : EtfSymbol.values()) {
+			symbolList.add(symbol.name());
+		}
+		
+		// 심볼리스트 To 배열
+		String[] symbols = symbolList.toArray(new String[symbolList.size()]);
+		return stockComponent.getStocks(symbols);
 	}
-	
-	
 	
 	/**
 	 * ETFs 주가 최근 1년치 초기 구축 (1회성)
@@ -105,6 +175,7 @@ public class StockDataScheduler {
 				stockHistory.setVolume(data.getVolume());
 				LocalDate date = LocalDateTime.ofInstant(data.getDate().toInstant(), ZoneId.systemDefault()).toLocalDate();
 				stockHistory.setStockDate(date);
+				stockHistory.setUpdateTime(LocalDateTime.now());
 				
 				// rsi
 				// rsi 계산을 위한 상승폭 또는 하락폭 큐에 저장
@@ -165,7 +236,6 @@ public class StockDataScheduler {
 					queue.add(upDownMap);
 				
 				beforeClose = nowClose;
-				
 				stockHistoryMapper.insertIgnore(stockHistory);
 			}
 		}

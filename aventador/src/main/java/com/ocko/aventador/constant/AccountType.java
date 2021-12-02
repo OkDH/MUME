b3c607af74ac796edacc6cf97e9c1cac7aa0e182
ock <ocko112@naver.com>
2021-12-03 00:29:48 +0900

    2021.12.03 무매 작업 중

---
+++ aventador/src/main/java/com/ocko/aventador/constant/AccountType.java
@@ -0,0 +1,12 @@
+package com.ocko.aventador.constant;
+
+/**
+ * 계좌 Type
+ * @author ocko1
+ *
+ */
+public enum AccountType {
+	INFINITE,
+	TLP,
+	VR
+}
---
+++ aventador/src/main/java/com/ocko/aventador/model/ResponseError.java
@@ -0,0 +1,14 @@
+package com.ocko.aventador.model;
+
+public class ResponseError {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}

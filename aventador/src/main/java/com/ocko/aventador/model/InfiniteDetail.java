b3e820ad0a57113f38d6f3906a35cb2f18fb0660
ok <ok@DESKTOP-R801POO>
2021-12-06 13:54:54 +0900

    20211206 계좌현황

--- aventador/src/main/java/com/ocko/aventador/model/InfiniteDetail.java
+++ aventador/src/main/java/com/ocko/aventador/model/InfiniteDetail.java
@@ -1,13 +1,54 @@
 package com.ocko.aventador.model;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import com.ocko.aventador.dao.model.aventador.ViewInfiniteList;
+import com.ocko.aventador.dao.model.aventador.ViewTodayStock;
 
 public class InfiniteDetail extends ViewInfiniteList {
-
-	// 매입금액
-	public BigDecimal getBuyPrice() {
-		return getAveragePrice().multiply(new BigDecimal(getHoldingQuantity()));
+	
+	// 해당 심볼 주가 정보
+	private StockDetail stockDetail;
+	
+	// 평가금액 : 종가 * 보유수량
+	public BigDecimal getEvalPrice() {
+		if(stockDetail == null)
+			return null;
+		return stockDetail.getPriceClose().multiply(new BigDecimal(getHoldingQuantity()));
 	}
+	
+	// 손익금 : 평가금액 - 매입금액 - 수수료
+	public BigDecimal getIncome() {
+		if(stockDetail == null)
+			return null;
+		return getEvalPrice().subtract(getBuyPrice()).subtract(getFees());
+	}
+	
+	// 손익률 : 손익금 / 매입금액 * 100
+	public BigDecimal getIncomePer() {
+		if(stockDetail == null)
+			return null;
+		return getIncome().divide(getBuyPrice(), 8, RoundingMode.HALF_EVEN).multiply(new BigDecimal(100));
+	}
+	
+	// 진행률 : 매입금액 / 배정시드 * 100
+	public BigDecimal getProgressPer() {
+		return getBuyPrice().divide(getSeed(), 8, RoundingMode.HALF_EVEN).multiply(new BigDecimal(100));
+	}
+
+	/**
+	 * @return {@link #stockDetail}
+	 */
+	public StockDetail getStockDetail() {
+		return stockDetail;
+	}
+
+	/**
+	 * @param stockDetail {@link #stockDetail}
+	 */
+	public void setStockDetail(StockDetail stockDetail) {
+		this.stockDetail = stockDetail;
+	}
+		
 }

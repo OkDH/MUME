fcdec90612e77fe47373dd8d4e32499fba11342d
ok <ok@DESKTOP-R801POO>
2021-12-06 13:54:54 +0900

    20211206 계좌현황

--- aventador/src/main/java/com/ocko/aventador/service/infinite/InfiniteStockService.java
+++ aventador/src/main/java/com/ocko/aventador/service/infinite/InfiniteStockService.java
@@ -3,9 +3,11 @@ package com.ocko.aventador.service.infinite;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,26 +18,58 @@ import com.ocko.aventador.constant.TradeType;
 import com.ocko.aventador.dao.model.aventador.InfiniteHistory;
 import com.ocko.aventador.dao.model.aventador.InfiniteStock;
 import com.ocko.aventador.dao.model.aventador.InfiniteStockExample;
+import com.ocko.aventador.dao.model.aventador.ViewInfiniteList;
+import com.ocko.aventador.dao.model.aventador.ViewInfiniteListExample;
 import com.ocko.aventador.dao.persistence.aventador.InfiniteHistoryMapper;
 import com.ocko.aventador.dao.persistence.aventador.InfiniteStockMapper;
+import com.ocko.aventador.dao.persistence.aventador.ViewInfiniteListMapper;
+import com.ocko.aventador.model.InfiniteDetail;
+import com.ocko.aventador.model.StockDetail;
+import com.ocko.aventador.service.StockService;
 
 @Service
 public class InfiniteStockService {
 
+	@Autowired private StockService stockService;
 	@Autowired private InfiniteStockMapper infiniteStockMapper;
 	@Autowired private InfiniteHistoryMapper infiniteHistoryMapper;
+	@Autowired private ViewInfiniteListMapper viewInfiniteListMapper;
 	
 	/**
 	 * 무매 종목리스트 조회
+	 * @param memberId
 	 * @param params
 	 * @return
 	 */
-	public List<InfiniteStock> getStocks(Map<String, Object> params){
-		InfiniteStockExample example = new InfiniteStockExample();
-		
+	public List<InfiniteDetail> getStocks(int memberId, Map<String, Object> params){
+		ViewInfiniteListExample example = new ViewInfiniteListExample();
 		example.setOrderByClause("registered_date desc");
+		example.createCriteria().andMemberIdEqualTo(memberId);
+		if(params.get("accountId") != null) {
+			example.createCriteria().andAccountIdEqualTo(Integer.parseInt(params.get("accountId").toString()));
+		}
 		
-		return infiniteStockMapper.selectByExample(example);
+		// view 조회
+		List<ViewInfiniteList> list = viewInfiniteListMapper.selectByExample(example);
+		
+		// etf 주가 정보 조회
+		Map<String, StockDetail> stockMap = stockService.getEtfStocks();
+		
+		// 손익정보 등 값 추가
+		List<InfiniteDetail> infiniteStockList = new ArrayList<InfiniteDetail>();
+		for(ViewInfiniteList viewInfinite : list) {
+			InfiniteDetail infiniteDetail = new InfiniteDetail();
+			
+			// 객체복사
+			BeanUtils.copyProperties(viewInfinite, infiniteDetail);
+			
+			// etf 주가 정보 추가
+			infiniteDetail.setStockDetail(stockMap.get(viewInfinite.getSymbol()));
+			
+			infiniteStockList.add(infiniteDetail);
+		}
+		
+		return infiniteStockList;
 	}
 	
 	/**

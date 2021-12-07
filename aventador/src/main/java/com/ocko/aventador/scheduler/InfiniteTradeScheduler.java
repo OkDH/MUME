b863332c9714478fb824323f8ae4578776b3d177
ock <ocko112@naver.com>
2021-12-08 00:37:03 +0900

    20211208 무매 수정

--- aventador/src/main/java/com/ocko/aventador/scheduler/InfiniteTradeScheduler.java
+++ aventador/src/main/java/com/ocko/aventador/scheduler/InfiniteTradeScheduler.java
@@ -3,11 +3,15 @@
  */
 package com.ocko.aventador.scheduler;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.ocko.aventador.dao.model.aventador.ViewTodayStock;
+import com.ocko.aventador.dao.persistence.aventador.ViewTodayStockMapper;
 import com.ocko.aventador.job.InfiniteTradeJob;
 
 /**
@@ -17,12 +21,27 @@ import com.ocko.aventador.job.InfiniteTradeJob;
 @Component
 public class InfiniteTradeScheduler {
 	
-	private static final Logger log = LoggerFactory.getLogger(InfiniteTradeScheduler.class);
+	@Autowired private InfiniteTradeJob infiniteJob;
+	@Autowired private ViewTodayStockMapper viewTodayStockMapper;
 	
-	@Autowired InfiniteTradeJob infiniteJob;
-	
-	public void updateInfiniteHistorys() {
-
+	/**
+	 * 무한매수 매매내역 업데이트
+	 * 화~토 오전 6시 10분에 작동
+	 */
+	@Scheduled(cron="0 10 6 * * 2-6")
+	public void updateInfiniteHistory() {
+		boolean isOpen = false;
+		
+		// 전일 데이터 날짜로 전날 미장 오픈 했는지 확인
+		List<ViewTodayStock> list = viewTodayStockMapper.selectByExample(null);
+		for(ViewTodayStock stock : list){
+			if (stock.getStockDate().isEqual(LocalDate.now().minusDays(1))) {
+				isOpen = true;
+				break;
+			}
+		}
+		if(isOpen)
+			infiniteJob.updateHistory();
 	}
 
 }

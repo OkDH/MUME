11516ac9f5454d9f069fecc7020049a82142a17a
ok <ok@DESKTOP-R801POO>
2021-12-15 19:13:03 +0900

    20211215 차트 작업중

--- aventador/src/main/resources/static/js/infinite/dashboard.js
+++ aventador/src/main/resources/static/js/infinite/dashboard.js
@@ -44,6 +44,84 @@ app.controller("InfiniteDashboardController", function($scope, httpService, infi
 			return;
 		
 		console.log("profitMonthly :" , profitMonthly);
+		
+		var labels = profitMonthly.map(function(item){
+			return item.monthly;
+		});
+		var data = profitMonthly.map(function(item){
+			return item.profit;
+		});
+
+		// Area Chart Example
+		var ctx = document.getElementById("profitMonthlyChart");
+		var myLineChart = new Chart(ctx, {
+		  type: 'bar',
+		  data: {
+		  	labels: labels,
+		    datasets: [{
+		      label: "손익금",
+		      backgroundColor: 'rgba(255, 99, 132, 0.2)', //  rgba(54, 162, 235, 0.2)
+		      borderColor: 'rgb(255, 99, 132)', // rgb(54, 162, 235)
+		      borderWidth: 1,
+		      data: data
+		    }],
+		  },
+		  options: {
+			  maintainAspectRatio: false,
+			  layout: {
+			      padding: {
+			        left: 10,
+			        right: 25,
+			        top: 25,
+			        bottom: 0
+			      }
+			  },
+			  scales: {
+			      y: {
+			        ticks: {
+			          maxTicksLimit: 5,
+			          padding: 10,
+			          // Include a dollar sign in the ticks
+			          callback: function(value, index, values) {
+			            return '$' + value;
+			          }
+			        },
+			        grid: {
+			          color: "rgb(234, 236, 244)",
+			          zeroLineColor: "rgb(234, 236, 244)",
+			          drawBorder: false,
+			          borderDash: [2],
+			          zeroLineBorderDash: [2]
+			        }
+			      },
+			  },
+			  plugins:{
+				  legend: {
+					  display: false
+				  },
+				  tooltip: {
+					  backgroundColor: "rgb(255,255,255)",
+					  bodyColor: "#858796",
+					  titleMarginBottom: 10,
+					  titleColor: '#6e707e',
+					  titleFontSize: 14,
+					  borderColor: '#dddfeb',
+					  borderWidth: 1,
+					  padding: 12,
+					  displayColors: false,
+					  intersect: false,
+					  mode: 'index',
+					  caretPadding: 10,
+					  callbacks: {
+						  label: function(tooltipItem) {
+							  console.log(tooltipItem);
+							  return tooltipItem.dataset.label + ': $' + tooltipItem.raw;
+						  }
+					  }
+				  }
+			  }
+		  }
+		});
 	});
 	
 	// 종목별 손익현황 차트

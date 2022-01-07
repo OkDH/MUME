c331613a04c9a19b1d7d32e9040a9d89c66c270e
ok <ok@DESKTOP-R801POO>
2022-01-07 10:52:52 +0900

    20220107 자동 매매 JOB 오류 수정

--- aventador/src/test/java/com/ocko/aventador/InfiniteTradeJobTest.java
+++ aventador/src/test/java/com/ocko/aventador/InfiniteTradeJobTest.java
@@ -6,6 +6,7 @@ package com.ocko.aventador;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import com.ocko.aventador.job.InfiniteTradeJob;
 

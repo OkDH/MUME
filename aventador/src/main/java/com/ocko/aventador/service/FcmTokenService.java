ed9502032051dcda5e371ab1e50f9a52ab6fdc0f
ok <ok@DESKTOP-R801POO>
2022-01-18 18:50:17 +0900

    20220118 FCM 토큰 작업중

--- aventador/src/main/java/com/ocko/aventador/service/FcmTokenService.java
+++ aventador/src/main/java/com/ocko/aventador/service/FcmTokenService.java
@@ -5,4 +5,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class FcmTokenService {
 
+	/**
+	 * @param memberId
+	 * @return
+	 */
+	public String getMyToken(Integer memberId) {
+		
+		
+		return null;
+	}
+
 }

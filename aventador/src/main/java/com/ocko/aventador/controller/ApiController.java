3e01e01b6f827a0fbde19810707f43edfa41021b
ok <ok@DESKTOP-R801POO>
2022-01-18 18:50:17 +0900

    20220118 FCM 토큰 작업중

--- aventador/src/main/java/com/ocko/aventador/controller/ApiController.java
+++ aventador/src/main/java/com/ocko/aventador/controller/ApiController.java
@@ -1,16 +1,27 @@
 package com.ocko.aventador.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.ocko.aventador.dao.model.aventador.MemberInfo;
+import com.ocko.aventador.service.AuthenticationService;
+import com.ocko.aventador.service.FcmTokenService;
+
 @Controller
 public class ApiController {
+	
+	@Autowired private AuthenticationService authenticationService;
+	@Autowired private FcmTokenService fcmTokenService;
 
 	@RequestMapping(value = "/api/member/token", method = RequestMethod.GET)
 	public @ResponseBody String getMemberTocken() {
-		return null;
+		MemberInfo memberInfo = authenticationService.getCurrentMember();
+		if(memberInfo == null)
+			return null;
+		return fcmTokenService.getMyToken(memberInfo.getMemberId());
 	}
 	
 }

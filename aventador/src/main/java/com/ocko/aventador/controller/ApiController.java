4fbbb975bea899c38f9a5a847155eac32f4f2fa2
ok <ok@DESKTOP-R801POO>
2022-01-19 18:16:33 +0900

    20220119 fcm토큰 관련 작업 중

--- aventador/src/main/java/com/ocko/aventador/controller/ApiController.java
+++ aventador/src/main/java/com/ocko/aventador/controller/ApiController.java
@@ -1,10 +1,15 @@
 package com.ocko.aventador.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ocko.aventador.dao.model.aventador.MemberInfo;
 import com.ocko.aventador.service.AuthenticationService;
@@ -16,12 +21,16 @@ public class ApiController {
 	@Autowired private AuthenticationService authenticationService;
 	@Autowired private FcmTokenService fcmTokenService;
 
-	@RequestMapping(value = "/api/member/token", method = RequestMethod.GET)
-	public @ResponseBody String getMemberTocken() {
+	/**
+	 * fcm 토큰 저장 
+	 * @return
+	 */
+	@RequestMapping(value = "/api/member/fcm-token", method = RequestMethod.POST)
+	public ResponseEntity<Boolean> updateFcmTocken(@RequestBody Map<String, Object> params) {
 		MemberInfo memberInfo = authenticationService.getCurrentMember();
 		if(memberInfo == null)
 			return null;
-		return fcmTokenService.getMyToken(memberInfo.getMemberId());
+		fcmTokenService.updateFcmToken(memberInfo.getMemberId(), params);
+		return new ResponseEntity<Boolean>(HttpStatus.OK);
 	}
-	
 }
--- aventador/src/main/java/com/ocko/aventador/service/FcmTokenService.java
+++ aventador/src/main/java/com/ocko/aventador/service/FcmTokenService.java
@@ -1,18 +1,37 @@
 package com.ocko.aventador.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.ocko.aventador.component.SimpleTokenComponent;
+import com.ocko.aventador.dao.model.aventador.MemberSetting;
+import com.ocko.aventador.dao.model.aventador.MemberSettingExample;
+import com.ocko.aventador.dao.persistence.aventador.MemberSettingMapper;
+import com.ocko.aventador.model.api.ResponseDto;
 
 @Service
 public class FcmTokenService {
+	
+	@Autowired private MemberSettingMapper memberSettingMapper;
 
 	/**
-	 * @param memberId
-	 * @return
+	 * FCM 토큰 등록
 	 */
-	public String getMyToken(Integer memberId) {
+	public Boolean updateFcmToken(Integer memberId, Map<String, Object> params) {
+		if(params.get("fcmToken") == null)
+			return false;
 		
+		MemberSetting updateSetting = new MemberSetting();
+		updateSetting.setMemberId(memberId);
+		updateSetting.setFcmToken(params.get("fcmToken").toString());
 		
-		return null;
+		memberSettingMapper.updateByPrimaryKeySelective(updateSetting);
+		
+		return true;
 	}
 
 }

fd45686325fe80b1d0d17d655cb6148309269b87
ok <ok@DESKTOP-R801POO>
2022-01-27 14:26:33 +0900

    20220127 시장지수

--- aventador/src/main/java/com/ocko/aventador/constant/MarketIndex.java
+++ aventador/src/main/java/com/ocko/aventador/constant/MarketIndex.java
@@ -1,30 +1,30 @@
 package com.ocko.aventador.constant;
 
 public enum MarketIndex {
-	DJI("^DJI"),
-	IXIC("^IXIC"),
-	GSPC("^GSPC"),
-	SOX("^SOX"),
+	DJI("^DJI"), // 다우산업
+	IXIC("^IXIC"), // 나스닥
+	GSPC("^GSPC"), // S&P500
+	SOX("^SOX"), // 필라델피아 반도체
 	
-	ESF("ES=F"),
-	NQF("NQ=F"),
-	YMF("YM=F"),
-	RTYF("RTY=F"),
+	ESF("ES=F"), // 다운산업 선물
+	NQF("NQ=F"), // 나스닥 선물
+	YMF("YM=F"), // S&P 500 선물
+	RTYF("RTY=F"), // 러셀 2000 선물
 	
-	KRWX("KRW=X"),
-	CLF("CL=F"),
-	GCF("GC=F"),
-	SIF("SI=F"),
+	KRWX("KRW=X"), // 환율
+	CLF("CL=F"), // 원가
+	GCF("GC=F"), // 금
+	SIF("SI=F"), // 은
 	
-	TNX("^TNX"), 
-	VIX("^VIX"), 
-	KS11("^KS11"),
-	KQ11("^KQ11"),
+	TNX("^TNX"), // 미국 국채 10년물 금리
+	VIX("^VIX"), // 변동성지수
+	KS11("^KS11"), // 코스피
+	KQ11("^KQ11"), // 코스닥
 	
-	CNSS("000001.SS"),
-	N225("^N225"),
-	GDAXI("^GDAXI"),
-	BTCKRW("BTC-KRW");
+	CNSS("000001.SS"), // 상해종합주가지수
+	N225("^N225"), // 니케이 225
+	STOXX("^STOXX50E"), // 유로스톡스50
+	BTCKRW("BTC-KRW"); // 비트코인
 	
 	private final String symbol;
 	
--- aventador/src/main/resources/static/js/public/market-index.js
+++ aventador/src/main/resources/static/js/public/market-index.js
@@ -2,27 +2,9 @@ app.controller("MarketIndexController", function($scope, $timeout, $q, stockServ
 	
 	var marketIndex = this;
 	
-//	marketIndex = {
-//			DJI : { symbol : "^DJI"	},
-//			IXIC : { symbol : "^IXIC" },
-//			GSPC : { symbol : "^GSPC" },
-//			SOX : { symbol : "^SOX" },
-//			ESF : { symbol : "ES=F" },
-//			NQF : { symbol : "NQ=F" },
-//			YMF : { symbol : "YM=F" },
-//		};
-	
-	// 시장지수
-	var symbols = ["^DJI", "^IXIC", "^GSPC", "^SOX",
-					"ES=F", "NQ=F", "YM=F", "RTY=F", 
-					"KRW=X", "CL=F", "GC=F", "SI=F", 
-					"^TNX", "^VIX", "^KS11", "^KQ11",
-					"000001.SS", "^N225", "^GDAXI", "BTC-KRW"];
-	
-	stockService.getStocks(symbols.join(",")).then(function(data){
+	stockService.getMarketIndex().then(function(data){
 		angular.forEach(data, function(value, key){
-			marketIndex[key] = {}
-			marketIndex[key].stock = value;
+			marketIndex[key] = value;
 		});
 	});
 	

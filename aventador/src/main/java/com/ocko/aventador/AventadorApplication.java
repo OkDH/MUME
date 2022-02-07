300c857113ff4ac41af585a975f7619f5f652207
ock <ocko112@naver.com>
2022-02-07 23:31:03 +0900

    20220207

--- aventador/src/main/java/com/ocko/aventador/AventadorApplication.java
+++ aventador/src/main/java/com/ocko/aventador/AventadorApplication.java
@@ -1,14 +1,20 @@
 package com.ocko.aventador;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class AventadorApplication {
+public class AventadorApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AventadorApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AventadorApplication.class, args);
 	}

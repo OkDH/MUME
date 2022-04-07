9bc8e35c110c2028a40ea70045972d039b249c82
ok <ok@DESKTOP-R801POO>
2022-04-07 14:02:24 +0900

    20220407 보고서 완료

--- aventador/src/main/resources/static/js/infinite/account.js
+++ aventador/src/main/resources/static/js/infinite/account.js
@@ -469,6 +469,8 @@ app.controller("InfiniteAccountController", function($scope, $filter, httpServic
 		});
 	}
 	
+	infiniteAccount.report = {};
+	
 	// 보고서 차트
 	$scope.$watch("infiniteAccount.viewStock.detail", function(detail){
 		if(!detail)
@@ -476,11 +478,291 @@ app.controller("InfiniteAccountController", function($scope, $filter, httpServic
 		
 		console.log(detail);
 		
-		// lable
+		var stockList = detail.stockList;
+		
+		// 일자별로 map 만들기
+		var datas = {};
+		detail.averagePriceList.forEach(function(item){
+			datas[item.tradeDate] = item;
+		});
+		
+		// label
+		var labels = [];
+		
+		// 평단가 
+		var averagePriceList = [];
+		// 큰수매수
+		var bigBuyList = [];
+		// 종가
+		var closeList = [];
+		// 5% 매도
+		var sellList1 = [];
+		// 10% 매도
+		var sellList2 = [];
+		// 고가
+		var highList = [];
+		
+		// 데이터 리스트 채우기
+		detail.stockList.forEach(function(item){
+			// label
+			labels.push(item.stockDate);
+			
+			// 종가, 고가
+			closeList.push(item.priceClose);
+			highList.push(item.priceHigh);
+			
+			// 평단가
+			var averagePrice;
+			if(datas[item.stockDate])
+				averagePrice = datas[item.stockDate].averagePrice;
+			else
+				averagePrice = averagePriceList[averagePriceList.length-1];
+			
+			averagePriceList.push(averagePrice);
+			
+			// 큰수 매수
+			bigBuyList.push((averagePrice * 1.05).toFixed(2));
+			
+			// 5% 매도
+			sellList1.push((averagePrice * 1.0517).toFixed(2));
+			
+			// 10% 매도
+			sellList2.push((averagePrice * 1.1017).toFixed(2));
+		});
+		
 		
 		// 매수 흐름 차트
+		{
+			// 그려진 차트가 있다면 차트 삭제 후 다시 그리기
+			if(infiniteAccount.report.buyFlowChart)
+				infiniteAccount.report.buyFlowChart.destroy();
+			
+			// 차트 그리기
+			var ctx = document.getElementById("buyFlowChart");
+			infiniteAccount.report.buyFlowChart = new Chart(ctx, {
+			  type: 'line',
+			  data: {
+			    labels: labels.map(x => x.replaceAll("-", ".").substring(2)),
+			    datasets: [
+			    	{
+						label: '종가',
+						lineTension: 0.1,
+						borderWidth: 2,
+						pointRadius: 1,
+						fill: false,
+						borderColor: '#4e73df',
+						backgroundColor: '#4e73df',
+				        pointBackgroundColor: '#4e73df',
+				        pointHoverRadius: 3,
+						data: closeList
+					},
+			    	{
+			    		label: '평단매수',
+			    		lineTension: 0.1,
+			    		borderWidth: 2,
+			    		pointRadius: 0,
+			    		fill: false,
+			    		borderColor: '#f0ad4e',
+			    		backgroundColor: '#f0ad4e',
+			    		pointBackgroundColor: '#f0ad4e',
+			    		pointHoverRadius: 3,
+			    		data: averagePriceList
+			    	},
+			    	{
+			    		label: '큰수매수',
+			    		lineTension: 0.1,
+			    		borderWidth: 2,
+			    		pointRadius: 0,
+			    		fill: false,
+			    		borderColor: '#d9534f',
+			    		backgroundColor: '#d9534f',
+			    		pointBackgroundColor: '#d9534f',
+			    		pointHoverRadius: 3,
+			    		data: averagePriceList
+			    	}
+			    ],
+			  },
+			  options: {
+			    maintainAspectRatio: false,
+			    layout: {
+			      padding: {
+			        left: 0,
+			        right: 0,
+			        top: 10,
+			        bottom: 0
+			      }
+			    },
+			    scales: {
+			      xAxes: [{
+			        time: {
+			          unit: 'date'
+			        },
+			        gridLines: {
+			          display: false,
+			          drawBorder: false
+			        },
+			        ticks: {
+			        }
+			      }],
+			      yAxes: [{
+			        ticks: {
+			          padding: 10,
+			          // Include a dollar sign in the ticks
+			          callback: function(value, index, values) {
+			            return '$' + value;
+			          }
+			        },
+			        gridLines: {
+			          color: "rgb(234, 236, 244)",
+			          zeroLineColor: "rgb(234, 236, 244)",
+			          drawBorder: false,
+			          zeroLineBorderDash: [2]
+			        }
+			      }],
+			    },
+			    legend: {
+			      display: true,
+			      position: 'top'
+			    },
+			    tooltips: {
+			      titleMarginBottom: 10,
+				  titleFontColor: '#6e707e',
+				  titleFontSize: 14,
+			      backgroundColor: 'rgb(255,255,255)',
+			      bodyFontColor: '#858796',
+			      borderColor: '#dddfeb',
+			      borderWidth: 1,
+			      xPadding: 15,
+			      yPadding: 15,
+			      displayColors: false,
+			      mode:'index',	
+			      callbacks: {
+			        label: function(tooltipItem, chart) {
+			          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+			          return datasetLabel + ': ' + $filter('number')(tooltipItem.yLabel, 1) + '$';
+			        }
+			      }
+			    }
+			  }
+			});
+		}
+		
 		
 		// 매도 흐름 차트
+		{
+			// 그려진 차트가 있다면 차트 삭제 후 다시 그리기
+			if(infiniteAccount.report.sellFlowChart)
+				infiniteAccount.report.sellFlowChart.destroy();
+			
+			// 차트 그리기
+			var ctx = document.getElementById("sellFlowChart");
+			infiniteAccount.report.sellFlowChart = new Chart(ctx, {
+			  type: 'line',
+			  data: {
+			    labels: labels.map(x => x.replaceAll("-", ".").substring(2)),
+			    datasets: [
+			    	{
+						label: '고가',
+						lineTension: 0.1,
+						borderWidth: 2,
+						pointRadius: 1,
+						fill: false,
+						borderColor: '#4e73df',
+						backgroundColor: '#4e73df',
+				        pointBackgroundColor: '#4e73df',
+				        pointHoverRadius: 3,
+						data: highList
+					},
+			    	{
+			    		label: '5%매도',
+			    		lineTension: 0.1,
+			    		borderWidth: 2,
+			    		pointRadius: 0,
+			    		fill: false,
+			    		borderColor: '#f0ad4e',
+			    		backgroundColor: '#f0ad4e',
+			    		pointBackgroundColor: '#f0ad4e',
+			    		pointHoverRadius: 3,
+			    		data: sellList1
+			    	},
+			    	{
+			    		label: '10%매도',
+			    		lineTension: 0.1,
+			    		borderWidth: 2,
+			    		pointRadius: 0,
+			    		fill: false,
+			    		borderColor: '#d9534f',
+			    		backgroundColor: '#d9534f',
+			    		pointBackgroundColor: '#d9534f',
+			    		pointHoverRadius: 3,
+			    		data: sellList2
+			    	}
+			    ],
+			  },
+			  options: {
+			    maintainAspectRatio: false,
+			    layout: {
+			      padding: {
+			        left: 0,
+			        right: 0,
+			        top: 10,
+			        bottom: 0
+			      }
+			    },
+			    scales: {
+			      xAxes: [{
+			        time: {
+			          unit: 'date'
+			        },
+			        gridLines: {
+			          display: false,
+			          drawBorder: false
+			        },
+			        ticks: {
+			        }
+			      }],
+			      yAxes: [{
+			        ticks: {
+			          padding: 10,
+			          // Include a dollar sign in the ticks
+			          callback: function(value, index, values) {
+			            return '$' + value;
+			          }
+			        },
+			        gridLines: {
+			          color: "rgb(234, 236, 244)",
+			          zeroLineColor: "rgb(234, 236, 244)",
+			          drawBorder: false,
+			          zeroLineBorderDash: [2]
+			        }
+			      }],
+			    },
+			    legend: {
+			      display: true,
+			      position: 'top'
+			    },
+			    tooltips: {
+			      titleMarginBottom: 10,
+				  titleFontColor: '#6e707e',
+				  titleFontSize: 14,
+			      backgroundColor: 'rgb(255,255,255)',
+			      bodyFontColor: '#858796',
+			      borderColor: '#dddfeb',
+			      borderWidth: 1,
+			      xPadding: 15,
+			      yPadding: 15,
+			      displayColors: false,
+			      mode:'index',	
+			      callbacks: {
+			        label: function(tooltipItem, chart) {
+			          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+			          return datasetLabel + ': ' + $filter('number')(tooltipItem.yLabel, 1) + '$';
+			        }
+			      }
+			    }
+			  }
+			});
+		}
 		
 	}, true);
 	

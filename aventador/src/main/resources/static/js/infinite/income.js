fae772c1196ae88afb22f75de7d929c511b7f6ca
ock <ocko112@naver.com>
2022-04-14 01:23:37 +0900

    20220414 손익현황 상세 작업중

--- aventador/src/main/resources/static/js/infinite/income.js
+++ aventador/src/main/resources/static/js/infinite/income.js
@@ -1,4 +1,4 @@
-app.controller("InfiniteIncomeController", function($scope, httpService, infiniteService){
+app.controller("InfiniteIncomeController", function($scope, $filter, httpService, infiniteService){
 	var infiniteIncome = this;
 	
 	var now = new Date();
@@ -36,6 +36,21 @@ app.controller("InfiniteIncomeController", function($scope, httpService, infinit
 		$('#filterModal').modal("show");
 		// selectpicker
 		$("#filterModal #accountSelect").selectpicker("refresh");
+	}
+	
+	infiniteIncome.detail = {};
+	
+	// 손익현황 상세 모달
+	infiniteIncome.openDetailModal = function(item){
+		$('#detailModal').modal("show");
+		infiniteIncome.detail = angular.copy(item);
+		
+		infiniteService.getStock({accountId: item.accountId, infiniteId: item.infiniteId}).then(function(data){
+			if(!data)
+				return;
+			
+			infiniteIncome.detail.stock = data;
+		});
 	}
 	
 	$scope.$watch("infiniteIncome.filter", function(filter){
@@ -81,6 +96,176 @@ app.controller("InfiniteIncomeController", function($scope, httpService, infinit
 		infiniteService.promiseGetIncome("monthly", query).then(function(data){
 			infiniteIncome.incomeByMonthly = data;
 		});
+		
+	}, true);
+	
+	// 매도 흐름 차트
+	infiniteIncome.report = {};
+	
+	// 보고서 차트
+	$scope.$watch("infiniteIncome.detail.stock", function(stock){
+		if(!stock)
+			return;
+		
+		var stockList = stock.stockList;
+		
+		// 일자별로 map 만들기
+		var datas = {};
+		stock.averagePriceList.forEach(function(item){
+			datas[item.tradeDate] = item;
+		});
+		
+		// label
+		var labels = [];
+		
+		// 평단가 
+		var averagePriceList = [];
+		// 5% 매도
+		var sellList1 = [];
+		// 10% 매도
+		var sellList2 = [];
+		// 고가
+		var highList = [];
+		
+		// 데이터 리스트 채우기
+		stock.stockList.forEach(function(item){
+			// label
+			labels.push(item.stockDate);
+			
+			// 고가
+			highList.push(item.priceHigh);
+			
+			// 평단가
+			var averagePrice;
+			if(datas[item.stockDate])
+				averagePrice = datas[item.stockDate].averagePrice;
+			else
+				averagePrice = averagePriceList[averagePriceList.length-1];
+			
+			averagePriceList.push(averagePrice);
+			
+			// 5% 매도
+			sellList1.push((averagePrice * 1.0517).toFixed(2));
+			
+			// 10% 매도
+			sellList2.push((averagePrice * 1.1017).toFixed(2));
+		});
+		
+		// 매도 흐름 차트
+		{
+			// 그려진 차트가 있다면 차트 삭제 후 다시 그리기
+			if(infiniteIncome.report.sellFlowChart)
+				infiniteIncome.report.sellFlowChart.destroy();
+			
+			// 차트 그리기
+			var ctx = document.getElementById("sellFlowChart");
+			infiniteIncome.report.sellFlowChart = new Chart(ctx, {
+			  type: 'line',
+			  data: {
+			    labels: labels.map(x => x.replaceAll("-", ".").substring(2)),
+			    datasets: [
+			    	{
+						label: '고가',
+						lineTension: 0.1,
+						borderWidth: 2,
+						pointRadius: 2,
+						fill: false,
+						borderColor: '#4e73df',
+						backgroundColor: '#4e73df',
+				        pointBackgroundColor: '#4e73df',
+				        pointHoverRadius: 3,
+						data: highList
+					},
+			    	{
+			    		label: '5%매도',
+			    		lineTension: 0.1,
+			    		borderWidth: 2,
+			    		pointRadius: 1,
+			    		fill: false,
+			    		borderColor: '#f0ad4e',
+			    		backgroundColor: '#f0ad4e',
+			    		pointBackgroundColor: '#f0ad4e',
+			    		pointHoverRadius: 3,
+			    		data: sellList1
+			    	},
+			    	{
+			    		label: '10%매도',
+			    		lineTension: 0.1,
+			    		borderWidth: 2,
+			    		pointRadius: 1,
+			    		fill: false,
+			    		borderColor: '#d9534f',
+			    		backgroundColor: '#d9534f',
+			    		pointBackgroundColor: '#d9534f',
+			    		pointHoverRadius: 3,
+			    		data: sellList2
+			    	}
+			    ],
+			  },
+			  options: {
+			    maintainAspectRatio: false,
+			    layout: {
+			      padding: {
+			        left: 0,
+			        right: 0,
+			        top: 10,
+			        bottom: 0
+			      }
+			    },
+			    scales: {
+			      xAxes: [{
+			        time: {
+			          unit: 'date'
+			        },
+			        gridLines: {
+			          display: false,
+			          drawBorder: false
+			        },
+			        ticks: {
+			        }
+			      }],
+			      yAxes: [{
+			        ticks: {
+			          padding: 10,
+			          // Include a dollar sign in the ticks
+			          callback: function(value, index, values) {
+			            return '$' + value;
+			          }
+			        },
+			        gridLines: {
+			          color: "rgb(234, 236, 244)",
+			          zeroLineColor: "rgb(234, 236, 244)",
+			          drawBorder: false,
+			          zeroLineBorderDash: [2]
+			        }
+			      }],
+			    },
+			    legend: {
+			      display: true,
+			      position: 'top'
+			    },
+			    tooltips: {
+			      titleMarginBottom: 10,
+				  titleFontColor: '#6e707e',
+				  titleFontSize: 14,
+			      backgroundColor: 'rgb(255,255,255)',
+			      bodyFontColor: '#858796',
+			      borderColor: '#dddfeb',
+			      borderWidth: 1,
+			      xPadding: 15,
+			      yPadding: 15,
+			      displayColors: false,
+			      mode:'index',	
+			      callbacks: {
+			        label: function(tooltipItem, chart) {
+			          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+			          return datasetLabel + ': ' + $filter('number')(tooltipItem.yLabel, 1) + '$';
+			        }
+			      }
+			    }
+			  }
+			});
+		}
 		
 	}, true);
 });

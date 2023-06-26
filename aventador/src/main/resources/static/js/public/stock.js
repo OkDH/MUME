92811ffd0665978543390c791a4a4d7cadb21058
ocko112 <ocko112@gmail.com>
2023-06-27 00:16:09 +0900

    financial 차트 수정 및 보조지표 RSI 추가

--- aventador/src/main/resources/static/js/public/stock.js
+++ aventador/src/main/resources/static/js/public/stock.js
@@ -1,5 +1,7 @@
 app.controller("StockController", function($scope, $filter, httpService, stockService){
 	var ngStock = this;
+	var financialChart;
+	var rsiChart;
 
 	// 시장지수
 	stockService.getMarketIndex().then(function(data){
@@ -43,32 +45,216 @@ app.controller("StockController", function($scope, $filter, httpService, stockSe
 			endDate: endDate
 		}
 
+		// 차트 그리기
 		stockService.getEtfHistory(params).then(function(data){
-			console.log(data);
 
-			// 차트 그리기
-			var ctx = document.getElementById('financialChart').getContext('2d');
+			var financialData = []; // 주가 차트 데이터
+			var ma = 20; // 20일 평균
+			var sum = 0;
+			var avgData = []; // 이동평균 데이터
+			var rsiLabels = [];
+			var rsiData = []; // rsi 데이터
 
-			var barData = [];
-			data.forEach(function(item){
-				barData.push({
+
+			for (var i = 0; i < data.length; i++) {
+				var item = data[i];
+				financialData.push({
 					x: new Date(item.stockDate).valueOf(),
-					o: item.priceOpen,
-					h: item.priceHigh,
-					l: item.priceLow,
-					c: item.priceClose
+					o: item.priceOpen.toFixed(2),
+					h: item.priceHigh.toFixed(2),
+					l: item.priceLow.toFixed(2),
+					c: item.priceClose.toFixed(2)
 				});
-			});
 
-			var chart = new Chart(ctx, {
+				rsiLabels.push(new Date(item.stockDate).valueOf());
+				rsiData.push(item.rsi.toFixed(2));
+
+				// 이동평균 구하기
+				sum += item.priceClose;
+				if(i < ma-1)
+					avgData.push({x: new Date(item.stockDate).valueOf()});
+				else if(i == ma-1){
+					avgData.push({x: new Date(item.stockDate).valueOf(), y: (sum/ma).toFixed(2)});
+				} else {
+					sum -= data[i-(ma-1)].priceClose;
+					avgData.push({x: new Date(item.stockDate).valueOf(), y: (sum/ma).toFixed(2)});
+				}
+
+			}
+
+			var ctx = document.getElementById('financialChart').getContext('2d');
+			if(financialChart)
+				financialChart.destroy();
+			financialChart = new Chart(ctx, {
 				type: 'candlestick',
 				data: {
-					datasets: [{
-						label: 'CHRT - Chart.js Corporation',
-						data: barData
-					}]
+					datasets: [
+						{
+							label: 'OHLC',
+							data: financialData
+						},
+						{
+							type: 'line',
+							label: 'MA(20)',
+							data: avgData,
+							pointRadius: 0
+						}
+					]
+				},
+				options: {
+					layout: {
+						padding: {
+							left: 10
+						}
+					},
 				}
 			});
+
+			// rsi chart
+			var rsiCtx = document.getElementById('rsiChart').getContext('2d');
+			if(rsiChart)
+				rsiChart.destroy();
+			setTimeout(function(){ // 모달의 차트 element가 다 뜨기 전에 그리기 시작하면 오류가 발생함.
+				rsiChart = new Chart(rsiCtx, {
+					type: 'line',
+					data: {
+						labels: rsiLabels,
+						datasets: [
+							{
+								label: 'RSI',
+								data: rsiData,
+								pointRadius: 0,
+								fill: {
+									target:{
+										value: etf.baseRsi
+									},
+									below: (context) => {
+										const chart = context.chart;
+										const { ctx, chartArea, data, scales } = chart;
+										if(!chartArea) {
+											return null;
+										}
+										return belowGradient(ctx, chartArea, data, scales);
+									},
+									above: (context) => {
+										const chart = context.chart;
+										const { ctx, chartArea, data, scales } = chart;
+										if(!chartArea) {
+											return null;
+										}
+										return aboveGradient(ctx, chartArea, data, scales);
+									},
+								},
+								borderColor: (context) => {
+									const chart = context.chart;
+									const { ctx, chartArea, data, scales } = chart;
+									if(!chartArea) {
+										return null;
+									}
+									return getGradient(ctx, chartArea, data, scales);
+								},
+								tension: 0.4,
+							}
+						]
+					},
+					options: {
+						aspectRatio: 5,
+						layout: {
+							padding: {
+								left: 10
+							}
+						},
+						scales: {
+							x: {
+								type: 'time',
+								time: {
+									unit: 'day'
+								},
+								grid: {
+									display: false
+								},
+								ticks: {
+									display: false
+								}
+							},
+							y: {
+								grid: {
+									display: false
+								},
+							}
+						},
+						plugins: {
+							legend: { display: false },
+						}
+					},
+					plugins: [
+						{
+							id: 'dottedLine',
+							beforeDatasetsDraw(chart, args, pluginOptions) {
+								const { ctx, data, chartArea: {left, right, width}, scales: {x,y} } = chart;
+
+								const startingPoint = etf.baseRsi
+
+								ctx.save();
+								ctx.beginPath();
+								ctx.lineWidth = 1;
+								ctx.setLineDash([1, 5]);
+								ctx.strokeStyle = 'rgba(102,102,102,1)';
+								ctx.moveTo(left, y.getPixelForValue(etf.baseRsi));
+								ctx.lineTo(right, y.getPixelForValue(etf.baseRsi));
+								ctx.stroke();
+								ctx.closePath();
+								ctx.setLineDash([]);
+
+								ctx.beginPath();
+								ctx.fillStyle = 'rgba(102,102,102,1)';
+								ctx.fillRect(0, y.getPixelForValue(startingPoint) - 10, left, 20);
+								ctx.closePath();
+
+								ctx.font = '12px sans-serif';
+								ctx.fillStyle = 'white';
+								ctx.textBaseline = 'middle';
+								ctx.textAlign = 'center';
+								ctx.fillText(etf.baseRsi, left / 2, y.getPixelForValue(startingPoint));
+							}
+						}
+					]
+				});
+			}, 150);
+
+			// https://www.youtube.com/watch?v=Q8np-7coqho&list=PLc1g3vwxhg1UlBvTOSZ3VJUjC_s6S2cMD&index=1
+			function getGradient(ctx, chartArea, data, scales) {
+				const { left, right, top, bottom, width, height } = chartArea;
+				const { x, y } = scales;
+				const gradientBorder = ctx.createLinearGradient(0, 0, 0, bottom);
+				const shift = y.getPixelForValue(etf.baseRsi) / bottom;
+
+				console.log(shift)
+
+				gradientBorder.addColorStop(0, 'rgba(75,192,192,1)');
+				gradientBorder.addColorStop(shift, 'rgba(75,192,192,1)');
+				gradientBorder.addColorStop(shift, 'rgba(255,26,104,1)');
+				gradientBorder.addColorStop(1, 'rgba(255,26,104,1)');
+				return gradientBorder;
+			}
+
+			function belowGradient(ctx, chartArea, data, scales) {
+				const { left, right, top, bottom, width, height } = chartArea;
+				const { x, y } = scales;
+				const gradientBackground = ctx.createLinearGradient(0, y.getPixelForValue(etf.baseRsi), 0, bottom);
+				gradientBackground.addColorStop(0, 'rgba(255,26,104,0)');
+				gradientBackground.addColorStop(1, 'rgba(255,26,104,0.5)');
+				return gradientBackground;
+			}
+
+			function aboveGradient(ctx, chartArea, data, scales) {
+				const { left, right, top, bottom, width, height } = chartArea;
+				const { x, y } = scales;
+				const gradientBackground = ctx.createLinearGradient(0, y.getPixelForValue(etf.baseRsi), 0, top);
+				gradientBackground.addColorStop(0, 'rgba(75,192,192,0)');
+				gradientBackground.addColorStop(1, 'rgba(75,192,192,0.5)');
+				return gradientBackground;
+			}
 		});
 	});
 	
